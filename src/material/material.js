b7eb5e242877e20d5d6fba110f78ca8a91781d6e
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-10-11 09:50:00 +0300

    Refactor the `Material` properties

--- src/material/material.js
+++ src/material/material.js
@@ -1,20 +1,37 @@
-/**@import {WebGLRenderPipelineDescriptor} from '../core/index.js' */
+/**@import {WebGLRenderPipelineDescriptor, BlendDescriptor } from '../core/index.js' */
 import {
   createTexture,
   updateTextureData,
 } from "../function.js"
 import {
-  BlendMode,
-  PrimitiveTopology,
-  CullFace
+  CullFace,
+  FrontFaceDirection,
 } from "../constant.js"
 import { Uniform } from "../core/index.js"
 import { Texture } from "../texture/index.js"
+import { MeshKey } from "../objects/mesh.js"
 
 export class Material {
+  /**
+   * @type {FrontFaceDirection}
+   */
+  frontFace = FrontFaceDirection.CCW
+  /**
+   * @type {CullFace}
+   */
   cullFace = CullFace.Back
-  distBlendFunc = BlendMode.OneMinusSrcAlpha
-  srcBlendFunc = BlendMode.SrcAlpha
+  /**
+   * @type {boolean}
+   */
+  depthTest = true
+  /**
+   * @type {boolean}
+   */
+  depthWrite = true
+  /**
+   * @type {BlendDescriptor | undefined}
+   */
+  blend
   /**
    * @param {string} vshaderSrc
    * @param {string} fshaderSrc
@@ -39,13 +56,53 @@ export class Material {
    * @returns {PipelineKey}
    */
   getPipelineKey(key){
-    return /**@type {PipelineKey}*/(key)
-  }
+    let materialKey = MaterialKey.None
+    if(this.cullFace === CullFace.Front){
+      materialKey |= MaterialKey.CullFaceFront
+    }else if(this.cullFace === CullFace.Back){
+      materialKey |= MaterialKey.CullFaceBack
+    }else if(this.cullFace === CullFace.FrontAndBack){
+      materialKey |= MaterialKey.CullFaceBoth
+    }
 
+    if(this.depthTest){
+      materialKey |= MaterialKey.DepthTest
+    }
+    if(this.depthWrite){
+      materialKey |= MaterialKey.DepthWrite
+    }
+    if(this.frontFace == FrontFaceDirection.CW){
+      materialKey |= MaterialKey.FrontFaceCW
+    }
+    
+    return /**@type {PipelineKey}*/(key | (materialKey << MeshKey.LastBit))
+  }
+  
   /**
    * @param {WebGLRenderPipelineDescriptor} descriptor 
    */
-  specialize(descriptor){}
+ specialize(descriptor){
+    // TODO: Incorporate blending to the pipeline key
+    descriptor.blend = this.blend
+    descriptor.cullFace = this.cullFace
+    descriptor.frontFace = this.frontFace
+    descriptor.depthTest = this.depthTest
+    descriptor.depthWrite = this.depthWrite
+  }
+}
+
+/**
+ * @enum {bigint}
+ */
+export const MaterialKey = {
+  LastBit: 63n,
+  None:0n,
+  CullFaceFront:1n << 0n,
+  CullFaceBack:1n << 1n,
+  CullFaceBoth:1n << 2n,
+  FrontFaceCW:1n << 3n,
+  DepthWrite:1n << 4n,
+  DepthTest:1n << 5n
 }
 
 /**
