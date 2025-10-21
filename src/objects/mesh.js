9a962a20609b1797c8ce88c8ce6689fc9ca52209
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-10-22 00:24:04 +0300

    Add `Skin` to enable mesh skinning on meshed objects

--- src/objects/mesh.js
+++ src/objects/mesh.js
@@ -15,6 +15,97 @@ import { Object3D } from "./object3d.js"
 import { Affine3 } from "../math/index.js"
 import { createTexture, createVAO, updateTextureData, updateTextureSampler } from "../function.js"
 import { Material, RawMaterial } from "../material/index.js"
+import { Bone3D } from "./bone.js";
+
+export class Skin {
+
+  // NOTE: Maybe this should not be here but available globally?
+  /**
+   * @type {Texture}
+   */
+  boneTexture = new Texture({
+    type: TextureType.Texture2D,
+    data: new Uint8Array([0, 0, 0, 0]).buffer,
+    width: 4,
+    height: 0,
+    format: TextureFormat.RGBA32Float,
+    generateMipmaps: false,
+  })
+
+  /**
+   * @type {Affine3}
+   */
+  bindMatrix = Affine3.identity()
+
+  /**
+   * @type {Affine3}
+   */
+  inverseBindMatrix = Affine3.identity()
+
+  /**
+   * @type {Bone3D[]}
+   */
+  bones = []
+
+  /**
+   * @type {Affine3[]}
+   */
+  inverseBindPose = []
+
+  constructor() { }
+
+  clone() {
+    const skin = new Skin()
+    skin.inverseBindPose = this.inverseBindPose
+    skin.bones = this.bones.slice()
+    return skin
+  }
+
+  setBindPose() {
+    for (let i = 0; i < this.inverseBindPose.length; i++) {
+      const element = this.inverseBindPose[i] || new Affine3();
+
+      element.copy(this.bones[i].transform.world).invert()
+      this.inverseBindPose[i] = element
+    }
+  }
+
+  updateTexture() {
+    const { bones, inverseBindPose } = this
+    const data = new Float32Array(this.bones.length * 16)
+
+    for (let i = 0; i < this.bones.length; i++) {
+      const offset = i * 16
+      const world = Affine3.multiply(
+        bones[i].transform.world,
+        inverseBindPose[i]
+      )/**/
+
+      // PERF: Remove last row as it is always constant
+      data[offset + 0] = world.a
+      data[offset + 1] = world.b
+      data[offset + 2] = world.c
+      data[offset + 3] = 0
+      data[offset + 4] = world.d
+      data[offset + 5] = world.e
+      data[offset + 6] = world.f
+      data[offset + 7] = 0
+      data[offset + 8] = world.g
+      data[offset + 9] = world.h
+      data[offset + 10] = world.i
+      data[offset + 11] = 0
+      data[offset + 12] = world.x
+      data[offset + 13] = world.y
+      data[offset + 14] = world.z
+      data[offset + 15] = 1
+    }
+
+    // TODO: Use the entire dimensions of the texture to pack values
+    this.boneTexture.height = this.bones.length
+    this.boneTexture.data = data.buffer
+    this.boneTexture.update()
+  }
+}
 
 /**
  * @template {Mesh} [T = Mesh]
