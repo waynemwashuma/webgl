9ca11b43040b1bde1eb37f2589805ab74689b0f3
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-10-23 07:28:23 +0300

    Add a split screen example

--- examples/samples/rendertarget/index.js
+++ examples/samples/rendertarget/index.js
@@ -1,5 +1,7 @@
 const meshTopology = new URL("./basic_canvas.js", import.meta.url)
+const splitScreen = new URL("./split_screen.js", import.meta.url)
 
 export default {
   "basic canvas": meshTopology,
+  "split screen": splitScreen
 }
