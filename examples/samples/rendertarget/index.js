95ce9f7f7857019b3e5c935170fde1746807e711
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-10-24 11:00:45 +0300

    Add a multiple view example

--- examples/samples/rendertarget/index.js
+++ examples/samples/rendertarget/index.js
@@ -1,7 +1,9 @@
 const meshTopology = new URL("./basic_canvas.js", import.meta.url)
 const splitScreen = new URL("./split_screen.js", import.meta.url)
+const multiViews = new URL("./multiple_views.js", import.meta.url)
 
 export default {
   "basic canvas": meshTopology,
-  "split screen": splitScreen
+  "split screen": splitScreen,
+  "multiple views": multiViews
 }
