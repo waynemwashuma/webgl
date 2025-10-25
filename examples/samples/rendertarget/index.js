70d9811585e023e32020c3a6b9578f033ccfa009
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-10-25 07:28:03 +0300

    Add a demo for split view

--- examples/samples/rendertarget/index.js
+++ examples/samples/rendertarget/index.js
@@ -1,9 +1,11 @@
 const meshTopology = new URL("./basic_canvas.js", import.meta.url)
 const splitScreen = new URL("./split_screen.js", import.meta.url)
 const multiViews = new URL("./multiple_views.js", import.meta.url)
+const splitView = new URL("./split_view.js", import.meta.url)
 
 export default {
   "basic canvas": meshTopology,
   "split screen": splitScreen,
+  "split view": splitView,
   "multiple views": multiViews
 }
