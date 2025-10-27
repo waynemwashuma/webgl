b09b2af8f2d9c5803272f9221a3a097a420c7b8b
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-10-27 19:34:12 +0300

    Add a render target texture demo

---
+++ examples/samples/rendertarget/image_target.js
@@ -0,0 +1,119 @@
+import {
+  MeshMaterial3D,
+  BasicMaterial,
+  BoxGeometry,
+  Quaternion,
+  WebGLRenderer,
+  TextureLoader,
+  PerspectiveProjection,
+  Camera,
+  WebGLCanvasSurface,
+  TextureType,
+  SkyBox,
+  Texture,
+  TextureFormat,
+  ImageRenderTarget,
+  Color
+} from "webgllis"
+
+const canvas = document.createElement('canvas')
+const surface = new WebGLCanvasSurface(canvas)
+const renderer = new WebGLRenderer()
+
+const renderTarget = new ImageRenderTarget({
+  width:1024,
+  height:1024,
+  color:[
+    new Texture({
+      type:TextureType.Texture2D,
+      format:TextureFormat.RGBA8Unorm
+    })
+  ],
+  internalDepthStencil:TextureFormat.Depth24PlusStencil8
+})
+
+const camera1 = new Camera()
+const camera2 = new Camera()
+
+const textureLoader = new TextureLoader()
+const texture = textureLoader.load({
+  paths: ["/assets/images/uv.jpg"],
+  textureSettings: {
+    flipY: true
+  }
+})
+const day = textureLoader.load({
+  paths: [
+    "/assets/images/skybox/miramar_right.png",
+    "/assets/images/skybox/miramar_left.png",
+    "/assets/images/skybox/miramar_top.png",
+    "/assets/images/skybox/miramar_bottom.png",
+    "/assets/images/skybox/miramar_back.png",
+    "/assets/images/skybox/miramar_front.png"
+  ],
+  type: TextureType.TextureCubeMap
+})
+const object1 = new MeshMaterial3D(new BoxGeometry(), new BasicMaterial({
+  mainTexture: texture
+}))
+const object2 = new MeshMaterial3D(new BoxGeometry(), new BasicMaterial({
+  mainTexture: renderTarget.color[0]
+}))
+const skyBox = new SkyBox({
+  day,
+  night:day
+})
+
+//set up the cameras
+renderTarget.clearColor.value = Color.CYAN.clone()
+camera1.target = renderTarget
+camera1.transform.position.z = 5
+camera2.transform.position.z = 5
+
+if (
+  camera1.projection instanceof PerspectiveProjection &&
+  camera2.projection instanceof PerspectiveProjection
+) {
+  camera1.projection.fov = Math.PI / 180 * 60
+  camera2.projection.fov = Math.PI / 180 * 60
+}
+
+document.body.append(canvas)
+updateView()
+addEventListener('resize', updateView)
+requestAnimationFrame(update)
+
+function update() {
+  renderer.render([object1], surface, camera1)
+  renderer.render([skyBox, object2], surface, camera2)
+
+  object1.transform.orientation.multiply(
+    Quaternion.fromEuler(Math.PI / 1000, Math.PI / 1000, 0)
+  )
+  object2.transform.orientation.multiply(
+    Quaternion.fromEuler(Math.PI / 1000, Math.PI / 1000, 0)
+  )
+
+  requestAnimationFrame(update)
+}
+
+function updateView() {
+  const fullWidth = innerWidth * devicePixelRatio
+  const fullHeight = innerWidth * devicePixelRatio
+
+  canvas.style.width = innerWidth + "px"
+  canvas.style.height = innerHeight + "px"
+  canvas.width = fullWidth
+  canvas.height = fullHeight
+
+  renderTarget.viewport.offset.set(0, 0)
+  renderTarget.viewport.size.set(1024, 1024)
+
+  if (
+    camera1.projection instanceof PerspectiveProjection &&
+    camera2.projection instanceof PerspectiveProjection
+  ) {
+    camera1.projection.aspect = fullWidth / fullHeight
+    camera2.projection.aspect = fullWidth / fullHeight
+  }
+}
