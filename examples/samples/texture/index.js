9ddeda142e763209c34398bac620965c456bc321
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-10-31 16:07:17 +0300

    Change separate sampler demo to texture wrap demo

--- examples/samples/texture/index.js
+++ examples/samples/texture/index.js
@@ -1,5 +1,5 @@
-const separateSamplers = new URL("./separateSampler.js", import.meta.url)
+const textureWrap = new URL("./textureWrap.js", import.meta.url)
 
 export default {
-  "separate samplers": separateSamplers
+  "texture wrap": textureWrap,
 }
