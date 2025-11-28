1396c4068e4329fe16925ddb059a6922dd99c618
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-11-28 15:38:31 +0300

    Add an example with multiple spot lights

--- examples/samples/lights/index.js
+++ examples/samples/lights/index.js
@@ -1,9 +1,11 @@
 const directional = new URL("./directional.js", import.meta.url)
 const point = new URL("./point.js", import.meta.url)
 const spot = new URL("./spot.js", import.meta.url)
+const multipleSpotLights = new URL("./multiple_spot_lights.js", import.meta.url)
 
 export default {
   "directional": directional,
   "point": point,
-  "spot": spot
+  "spot": spot,
+  "multiple spot lights": multipleSpotLights,
 }
