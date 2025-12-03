3e745fcf5903cbb8eb68b99d1956c20f8d7cc0b9
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-12-03 18:45:00 +0300

    Add an example of gltf model with materials

--- examples/samples/loader/index.js
+++ examples/samples/loader/index.js
@@ -1,7 +1,9 @@
 const objLoader = new URL("./objloader.js", import.meta.url)
 const gltfLoader = new URL("./gltfloader.js", import.meta.url)
+const gltfMaterial = new URL("./gltf_material.js", import.meta.url)
 
 export default {
   "obj": objLoader,
   "gltf": gltfLoader,
+  "gltf material": gltfMaterial
 }
