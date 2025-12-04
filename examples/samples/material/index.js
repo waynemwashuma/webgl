9dd4a541524c9a0cffb55112e5eb80db9cb24251
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-12-04 20:30:00 +0300

    Move standard material example to its folder

--- examples/samples/material/index.js
+++ examples/samples/material/index.js
@@ -1,9 +1,9 @@
+import standard from './standard/index.js'
 const cullFace = new URL("./cullface.js", import.meta.url)
 const materials = new URL("./materials.js", import.meta.url)
-const standard = new URL("./standard.js", import.meta.url)
 
 export default {
   "cull face": cullFace,
   "materials": materials,
-  "standard material": standard
+  "standard": standard
 }
