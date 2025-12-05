739cbb40df1d445771b2f49b7fb9ba7b4e73d731
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-12-05 20:15:00 +0300

    Make direction light a 3d object

--- src/light/directional.js
+++ src/light/directional.js
@@ -1,27 +1,16 @@
-import {
-  Color,
-  Vector3,
-  Transform3D
-} from '../math/index.js'
+import { Color, Vector3 } from '../math/index.js'
+import { Object3D } from '../objects/index.js'
 
-export class DirectionalLight {
-  transform = new Transform3D()
+export class DirectionalLight extends Object3D {
   intensity = 1.0
   color = new Color()
-  direction = new Vector3(0,0,1)
-  
-  /**
-   * @param {{ transform: Transform3D; }} [parent]
-   */
-  update(parent) {
-    this.transform.updateMatrix(parent?.transform)
-  }
-  
+  direction = new Vector3(0, 0, 1)
+
   /**
    */
   pack() {
     const direction = this.direction.clone()
-    
+
     return [
       ...this.color,
       ...direction,
