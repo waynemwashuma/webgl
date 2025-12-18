3a9841f5628444b9ee32cca35e15d0b8e54f9f61
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-12-18 19:09:47 +0300

    Implement shadows for point light on lambert material

--- src/light/point.js
+++ src/light/point.js
@@ -1,11 +1,17 @@
 import { Color, Vector3 } from '../math/index.js'
-import { Object3D } from '../objects/object3d.js'
+import { Object3D } from '../objects/index.js'
+import { SpotLightShadow } from './shadow/index.js'
 
 export class PointLight extends Object3D {
   color = new Color()
   radius = 1.0
   decay = 2
   intensity = 1.0
+
+  /**
+   * @type {SpotLightShadow | undefined}
+   */
+  shadow
 
   pack() {
     const worldPosition = new Vector3(
