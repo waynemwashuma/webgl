47bee1ff63e1bff235cad4966f4d4578599119ff
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-12-19 23:56:00 +0300

    Add a depth render target example

--- examples/samples/rendertarget/index.js
+++ examples/samples/rendertarget/index.js
@@ -3,11 +3,13 @@ const splitScreen = new URL("./split_screen.js", import.meta.url)
 const multiViews = new URL("./multiple_views.js", import.meta.url)
 const splitView = new URL("./split_view.js", import.meta.url)
 const imageTarget = new URL("./image_target.js", import.meta.url)
+const depthImage = new URL("./depth_texture.js", import.meta.url)
 
 export default {
   "basic canvas": meshTopology,
   "split screen": splitScreen,
   "split view": splitView,
   "multiple views": multiViews,
-  "image target":imageTarget
+  "image target":imageTarget,
+  "depth target":depthImage
 }
