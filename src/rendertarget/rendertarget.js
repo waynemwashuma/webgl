c133ced73c0f16ef03c08289fe946c7d1eedaafe
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-12-22 19:58:00 +0300

    Refactor frame buffer caching

--- src/rendertarget/rendertarget.js
+++ src/rendertarget/rendertarget.js
@@ -5,7 +5,19 @@ import { ViewRectangle, Range } from "../utils/index.js";
  * @abstract
  */
 export class RenderTarget {
-
+  /**
+   * @protected
+   * @type {boolean}
+   */
+  _change = false
+  /**
+   * @type {number}
+   */
+  #width = 0
+   /**
+   * @type {number}
+   */
+  #height = 0
   /**
    * @type {Color | undefined}
    */
@@ -30,4 +42,34 @@ export class RenderTarget {
    * @type {Range | undefined}
    */
   depthRange
+
+  /**
+   * @param {number} width
+   * @param {number} height
+   */
+  constructor(width, height){
+    this.#width = width
+    this.#height = height
+  }
+  get width(){
+    return this.#width
+  }
+  set width(value){
+    this.#width = value
+    this._change = true
+  }
+  get height(){
+    return this.#height
+  }
+  set height(value){
+    this.#height = value
+    this._change = true
+  }
+
+  changed(){
+    const prev = this._change
+    this._change = false
+
+    return prev
+  }
 }
