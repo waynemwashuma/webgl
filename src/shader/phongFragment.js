12c488cb7e22b2bb4eff138564944119c67dbcc3
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-12-30 20:07:00 +0300

    Implement light shadows on phong material
    Adds shadows to point, spot and directional lights

--- src/shader/phongFragment.js
+++ src/shader/phongFragment.js
@@ -35,6 +35,12 @@ export const phongFragment =
   uniform SpotLightBlock {
     SpotLights spot_lights;
   };
+  #ifdef MAX_SHADOW_CASTERS
+    uniform ShadowCasterBlock {
+      Shadow shadow_casters[MAX_SHADOW_CASTERS];
+    };
+    uniform sampler2DArray shadow_atlas;
+  #endif
   uniform sampler2D mainTexture;
   
   out vec4 fragment_color;
@@ -55,21 +61,29 @@ export const phongFragment =
     int directional_light_count = min(directional_lights.count,MAX_DIRECTIONAL_LIGHTS);
     int point_light_count = min(point_lights.count,MAX_POINT_LIGHTS);
     int spot_light_count = min(spot_lights.count,MAX_SPOT_LIGHTS);
-
-    vec3 ambient = ambient_light.color.rgb * ambient_light.intensity;
     
-    vec3 accumulate_light_contribution = vec3(0.0,0.0,0.0);
+    vec3 total_exitance = vec3(0.0);
     for (int i = 0; i < directional_light_count; i++) {
       DirectionalLight light = directional_lights.lights[i];
       vec3 reflection_direction = reflect(light.direction, normal);
-      
+      vec3 irradiance = light.color.rgb;
       //Remember you set the dir to negative because light direction is the opposite direction of dir.
-      float diffuse_brightness = calculate_brightness(normal,-light.direction);
-      vec3 diffuse = base_color * light.color.rgb * diffuse_brightness * light.intensity;
+      float diffuse_brightness = calculate_brightness(normal,-light.direction);    
+      
+      #ifdef MAX_SHADOW_CASTERS
+        if(light.shadow_index != -1){
+          Shadow shadow = shadow_casters[light.shadow_index];
+          
+          irradiance *= shadow_contribution_2d(shadow, shadow_atlas, v_position, diffuse_brightness);
+        }
+      #endif
+      
+      vec3 diffuse = base_color * irradiance * diffuse_brightness * light.intensity;
     
       float specular_brightness = calculate_brightness(reflection_direction,view_direction);
-      vec3 specular = pow(specular_brightness,material.specularShininess) * light.color.rgb * material.specularStrength;
-      accumulate_light_contribution += specular + diffuse;
+      vec3 specular = pow(specular_brightness,material.specularShininess) * irradiance * material.specularStrength;
+
+      total_exitance += specular + diffuse;
     }
     for (int i = 0; i < point_light_count; i++) {
       PointLight light = point_lights.lights[i];
@@ -79,12 +93,21 @@ export const phongFragment =
       float attenuation = attenuate_point_light(distance, light.radius, light.intensity, light.decay);
       float brightness = calculate_brightness(normal, direction);
       vec3 irradiance = light.color.rgb * attenuation;
+
+      #ifdef MAX_SHADOW_CASTERS
+        if(light.shadow_index != -1){
+          Shadow shadow = shadow_casters[light.shadow_index];
+          
+          irradiance *= shadow_contribution_cube(shadow, shadow_atlas, v_position, brightness);
+        }
+      #endif
+
       vec3 reflection_direction = reflect(direction, normal);
       vec3 diffuse_exitance = base_color * brightness * irradiance;
       
       float specular_brightness = calculate_brightness(reflection_direction, -view_direction);
       vec3 specular_exitance = pow(specular_brightness,material.specularShininess) * irradiance * material.specularStrength;
-      accumulate_light_contribution += specular_exitance + diffuse_exitance;
+      total_exitance += specular_exitance + diffuse_exitance;
     }
 
     for (int i = 0; i < spot_light_count; i++) {
@@ -95,14 +118,25 @@ export const phongFragment =
       float attenuation = attenuate_spot_light(light, direction, distance);
       float brightness = calculate_brightness(normal, direction);
       vec3 irradiance = light.color.rgb * attenuation;
+      
+      #ifdef MAX_SHADOW_CASTERS
+        if(light.shadow_index != -1){
+          Shadow shadow = shadow_casters[light.shadow_index];
+          
+          irradiance *= shadow_contribution_2d(shadow, shadow_atlas, v_position, brightness);
+        }
+      #endif
+
       vec3 reflection_direction = reflect(direction, normal);
       vec3 diffuse_exitance = base_color * brightness * irradiance;
-      
       float specular_brightness = calculate_brightness(reflection_direction, -view_direction);
       vec3 specular_exitance = pow(specular_brightness,material.specularShininess) * irradiance * material.specularStrength;
-      accumulate_light_contribution += specular_exitance + diffuse_exitance;
+
+      total_exitance += specular_exitance + diffuse_exitance;
     }
-    vec3 final_color = base_color * ambient + accumulate_light_contribution;
+    
+    vec3 ambient = base_color * ambient_light.color.rgb * ambient_light.intensity;
+    vec3 final_color = ambient + total_exitance;
     
     fragment_color = vec4(final_color, opacity);
   }
